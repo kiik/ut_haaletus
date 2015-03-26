1844c12f0c6e2194a140f2c2dac2abb0b7a2be1b
Meelik Kiik <kiik12@hotmail.com>
2015-03-26 17:58:48 +0000

    Enable regular routes on https

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var checkAuth = function(cert, callback) {
 
 app = module.exports = express();
 //app.use(clientCertificateAuth(checkAuth));
-//app.use(kraken(options));
+app.use(kraken(options));
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
