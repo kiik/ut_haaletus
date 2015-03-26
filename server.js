28cea0df28ee9ea4d3da4ae42ecd3eb6eaef4409
Meelik Kiik <kiik12@hotmail.com>
2015-03-26 16:21:13 +0000

    Implement certificate authentication functionality

--- server.js
+++ server.js
@@ -2,15 +2,25 @@
 
 var app = require('./index');
 var http = require('http');
-
-
+var https = require('https');
 var server;
+
+var fs = require('fs');
+
+var opts = {
+    key : fs.readFileSync('certs/server.key'),
+    cert : fs.readFileSync('certs/server.crt'),
+    ca : fs.readFileSync('certs/ca.crt'),
+    passphrase : "password",
+    requestCert : true,
+    rejectUnauthorized: false,
+}
 
 /*
  * Create and start HTTP server.
  */
 
-server = http.createServer(app);
+server = https.createServer(opts, app);
 server.listen(process.env.PORT || 8000);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
