486ba271d7d29c54b59f42718fb64f0ca6321150
Kelian Kaio <kelian14@gmail.com>
2015-05-24 18:15:23 +0300

    Added links to profile page

--- controllers/profile.js
+++ controllers/profile.js
@@ -7,12 +7,25 @@ module.exports = function (router) {
     router.get('/data', function (req, res) {
         db.User.find({ where: { ssn: res.locals.user.ssn }}).then(function(user) {
             db.Vote.find({where: {UserId : user.id}}).then(function(data) {
-                res.json({
-                    first: user.first_name,
-                    last: user.last_name,
-                    hasVoted: data? true : false,
-                    isCandidate: user.OrganizationId? true : false,
-                });
+                if(data) {
+                    db.Application.find({where:{id: data.ApplicationId}}).then(function(candidate) {
+                        res.json({
+                            first: user.first_name,
+                            last: user.last_name,
+                            id: candidate.UserId,
+                            hasVoted: true,
+                            isCandidate: user.OrganizationId? true : false,
+                        });
+                    });
+                } else {
+                    res.json({
+                        first: user.first_name,
+                        last: user.last_name,
+                        id: "none",
+                        hasVoted: false,
+                        isCandidate: user.OrganizationId? true : false,
+                    });
+                }
             });
         });
     });
